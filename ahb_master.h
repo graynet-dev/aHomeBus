6c4b3cae2d3f7532e0c8560af6e5342f4974033e
graynet-dev <51855711+graynet-dev@users.noreply.github.com>
2020-03-08 11:43:09 +0300

    Create ahb_master.h

---
+++ ahb_master.h
@@ -0,0 +1,16 @@
+#ifndef AHB_MASTER_H
+#define AHB_MASTER_H
+    #include "ahb.h"
+    #include "ahb_proto.h"
+    
+class AHB_MASTER { //: public AHB_MCOMM
+public:            
+            bool NodeGuard_OK[255][3] = {{ 0 }}; //[2]
+            
+            AHB_MASTER(uint8_t id); 
+             
+            // Up Time remote node 
+            uint8_t ut_rn[256][6];    
+};
+    
+#endif /* AHB_MASTER_H */
