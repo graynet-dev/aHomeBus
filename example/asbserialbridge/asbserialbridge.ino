3cbc8a411a224675f1b75a26d101a6818f35a86e
graynet-dev <51855711+graynet-dev@users.noreply.github.com>
2020-03-08 11:43:44 +0300

    Create asbserialbridge.ino

---
+++ example/asbserialbridge/asbserialbridge.ino
@@ -0,0 +1,78 @@
+#include "asb.h"
+
+/*
+ * Configuration
+ */
+
+#define FIRMWARE_VERSION                        "1.0.0"
+
+#define ASB_BRIDGE_NODE_ID                      0x0001
+#define ASB_NODE_ID                             ASB_BRIDGE_NODE_ID
+
+#define PIN_CAN_CS                              53
+#define PIN_CAN_INT                             2
+
+/*
+ * Initialisation
+ */
+
+ASB asb0(ASB_NODE_ID);
+ASB_CAN asbCan0(PIN_CAN_CS, CAN_125KBPS, MCP_8MHz, PIN_CAN_INT);
+ASB_UART asbUart0(Serial);
+
+/*
+ * Setup
+ */
+
+void setup()
+{
+    Serial.begin(115200);
+
+    char buffer[64] = {0};
+    sprintf(buffer, "setup(): The node '0x%04X' was powered up.", ASB_NODE_ID);
+    Serial.println(buffer);
+
+    setupCanBus();
+    setupUart();
+}
+
+void setupCanBus()
+{
+    Serial.println(F("setupCanBus(): Attaching CAN..."));
+
+    const char busId = asb0.busAttach(&asbCan0);
+
+    if (busId != -1)
+    {
+        Serial.println(F("setupCanBus(): Attaching CAN was successful."));
+    }
+    else
+    {
+        Serial.println(F("setupCanBus(): Attaching CAN failed!"));
+    }
+}
+
+void setupUart()
+{
+    Serial.println(F("setupUart(): Attaching UART..."));
+
+    const char busId = asb0.busAttach(&asbUart0);
+
+    if (busId != -1)
+    {
+        Serial.println(F("setupUart(): Attaching UART was successful."));
+    }
+    else
+    {
+        Serial.println(F("setupUart(): Attaching UART failed!"));
+    }
+}
+
+/*
+ * Loop
+ */
+
+void loop()
+{
+    asb0.loop();
+}
