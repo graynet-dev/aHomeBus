f3a01ab7d8892110da55de6b035173b606ca42e3
graynet-dev <51855711+graynet-dev@users.noreply.github.com>
2020-03-08 11:43:05 +0300

    Create ahb_node.cpp

---
+++ ahb_node.cpp
@@ -0,0 +1,31 @@
+#ifndef AHB_NODE_C
+#define AHB_NODE_C
+    #include "ahb_node.h"
+
+AHB_NODE::AHB_NODE(uint8_t id){
+
+   void WTD_node(void);
+
+} 
+
+void AHB_NODE::WTD_node(unsigned long millis_){
+
+        if (millis_ - prevTimeHeartbeatNode > 10000) {
+          Serial.print(heartbeat_count);Serial.println(F(" - heartbeat_count"));
+          //Serial.println(F("+++++++++++++++++++++++++++++++++++++++"));
+          heartbeat_count--; 
+          prevTimeHeartbeatNode = millis_;
+        }
+        
+        if (heartbeat_count > 10) {
+          heartbeat_count=10;
+        }
+        
+        if (heartbeat_count < 1) {
+          Serial.println(F("No receive Heartbeat from all, reboot node"));
+           while(1){}
+        }
+}
+  
+    
+#endif /* AHB_NODE_C */
