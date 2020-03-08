904aa44a0ea312bfb04e41181fb6ce3d985f8f32
graynet-dev <51855711+graynet-dev@users.noreply.github.com>
2020-03-08 11:43:30 +0300

    Update ahb_eth.h

--- ahb_eth.h
+++ ahb_eth.h
@@ -22,28 +22,33 @@
 */
 #ifndef AHB_ETH_H
 #define AHB_ETH_H
-/**
+
   #include <Ethernet2.h>
   #include <EthernetUdp2.h>
+  #include "ahb.h"
+  #include <SPI.h> 
 
-  #define ETH_SS 10
-  #define SD_SS 4
+  //#define ETH_SS 10
+  //#define SD_SS 4
 
-class AHB_ETH : public EthernetClass{
+class AHB_ETH {
         private:
-        
+        AHB *_ahb;
+        EthernetClass _interface_eth;
         
         public:
-        EthernetClass _interface;
-        //EthernetServer server;
-        EthernetClient client;
-        AHB_ETH(uint8_t _cspin = 10);//, uint16_t server_port = 0);
-
-//   int begin(uint8_t *mac_address);
-//  void begin(uint8_t *mac_address, IPAddress local_ip);
-//  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server);
-//  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway);
-//  void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet);
+         EthernetClass _interface;
+         //EthernetClient client;
+         //EthernetServer server = EthernetServer(80);
+         AHB_ETH(uint8_t _cspin = 10);//, uint16_t server_port = 0);
+         void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet);
+         bool EthAttach(AHB *ahb);
+        
+      //int begin(uint8_t *mac_address);
+      //void begin(uint8_t *mac_address, IPAddress local_ip);
+      //void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server);
+      //void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway);
+      //void begin(uint8_t *mac_address, IPAddress local_ip, IPAddress dns_server, IPAddress gateway, IPAddress subnet);
 };
-*/
-#endif
+
+#endif //AHB_ETH_H
