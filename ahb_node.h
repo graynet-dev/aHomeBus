7b4e1ed165b18db977d7ca40ed7929c255f40a27
graynet-dev <51855711+graynet-dev@users.noreply.github.com>
2020-03-08 11:43:07 +0300

    Create ahb_node.h

---
+++ ahb_node.h
@@ -0,0 +1,19 @@
+#ifndef AHB_NODE_H
+#define AHB_NODE_H
+    #include "ahb.h"
+    #include "ahb_proto.h"
+    
+class AHB_NODE { 
+public:            
+            
+            AHB_NODE(uint8_t id); 
+            
+            void WTD_node(unsigned long millis_);
+            
+            unsigned long prevTimeHeartbeatNode = 0; //для таймера Heartbeat
+            
+            uint8_t heartbeat_count = 10; //количество циклов не приема Heartbeat
+  
+};
+    
+#endif /* AHB_NODE_H */
