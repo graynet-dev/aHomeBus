96bfb84abc16a9cd1420f7d3d495f7d4f75f2045
graynet-dev <51855711+graynet-dev@users.noreply.github.com>
2020-03-13 18:25:56 +0300

    Update ahb_node.h

--- ahb_node.h
+++ ahb_node.h
@@ -1,9 +1,11 @@
 #ifndef AHB_NODE_H
 #define AHB_NODE_H
-    #include "ahb.h"
+
+    //#include "ahb.h"
     #include "ahb_proto.h"
     
 class AHB_NODE { 
+
 public:            
             
             AHB_NODE(uint8_t id); 
@@ -15,5 +17,7 @@ public:
             uint8_t heartbeat_count = 10; //количество циклов не приема Heartbeat
   
 };
+
+class AHB_NODE2 {};
     
 #endif /* AHB_NODE_H */
