c0b6136ad3e5d5637ec6f5a614b997ddd32a24b7
graynet-dev <51855711+graynet-dev@users.noreply.github.com>
2020-03-22 03:21:48 +0300

    Update ahb_slave.h

--- ahb_slave.h
+++ ahb_slave.h
@@ -5,11 +5,15 @@
     
 class AHB_SLAVE { 
 public:            
-
+            /*
+            Массив со статусами удаленных узлов. Заполняется на основе ответов
+            */
             bool NodeGuard_OK[255][3] = {{ 0 }}; //[2]
             
-            AHB_SLAVE(uint8_t id);  
-          
+            //Конструктор
+            AHB_SLAVE(uint8_t id);
+  
+            // Up Time remote node
             uint8_t ut_rn[256][7];  
 };
     
